934d4d5d0f946f2ec8a27c1cee66080c5f41c67b
kaaveh <kaaveh@pm.me>
2022-12-13 17:53:42 +0330

    [refactor] hide bottomNav

--- app/src/main/java/ir/kaaveh/composenews/ui/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/composenews/ui/MainActivity.kt
@@ -5,15 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import ir.kaaveh.composenews.navigation.BottomNavItem
 import ir.kaaveh.composenews.navigation.ComposeNewsNavHost
-import ir.kaaveh.composenews.navigation.Destinations
 import ir.kaaveh.composenews.ui.component.BottomNavigationBar
 import ir.kaaveh.composenews.ui.component.items
 import ir.kaaveh.designsystem.theme.ComposeNewsTheme
@@ -24,16 +20,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeNewsTheme {
+
                 val navController = rememberNavController()
+                val backStackEntry = navController.currentBackStackEntryAsState()
+                val currentScreenRoute = backStackEntry.value?.destination?.route
+                val bottomNavVisible = items.any {
+                    it.route == currentScreenRoute
+                }
+
                 Scaffold(
                     bottomBar = {
-                        BottomNavigationBar(
-                            items = items,
-                            navController = navController,
-                            onItemClick = {
+                        if (bottomNavVisible)
+                            BottomNavigationBar(
+                                items = items,
+                                currentScreenRoute = currentScreenRoute
+                            ) {
                                 navController.navigate(it.route)
                             }
-                        )
                     }
                 ) { paddingValues ->
                     ComposeNewsNavHost(
