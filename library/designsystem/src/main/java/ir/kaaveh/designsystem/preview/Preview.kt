d5cd416c0ccb937c497d06dba7fa3be1b019b15d
kaaveh <kaaveh@pm.me>
2022-12-19 10:51:33 +0330

    [feature] preview in design system module

---
+++ library/designsystem/src/main/java/ir/kaaveh/designsystem/preview/Preview.kt
@@ -0,0 +1,18 @@
+package ir.kaaveh.designsystem.preview
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "Light", showBackground = true)
+@Preview(
+    name = "Dark",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+annotation class ThemePreviews
+
+@Preview(name = "Phone", device = "spec:width=411dp,height=891dp")
+@Preview(name = "Foldable", device = "spec:width=673.5dp,height=841dp,dpi=480")
+@Preview(name = "Tablet", device = "spec:width=1280dp,height=800dp,dpi=480")
+@Preview(name = "Desktop", device = "spec:width=1920dp,height=1080dp,dpi=480")
+annotation class DevicesPreviews
