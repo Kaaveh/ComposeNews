0c7fd862764348004cd44b28a2a8e2eeed8068d0
Baha <ali.bahaabadi@basalam.com>
2023-01-14 10:48:55 +0330

    [feature] add news dto to db.

--- data/news-local/src/main/java/ir/kaaveh/localdatasource/database/FavoriteNewsDatabase.kt
+++ data/news-local/src/main/java/ir/kaaveh/localdatasource/database/FavoriteNewsDatabase.kt
@@ -3,12 +3,13 @@ package ir.kaaveh.localdatasource.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ir.kaaveh.localdatasource.dto.FavoriteNewsDto
+import ir.kaaveh.localdatasource.dto.NewsDto
 
 @Database(
-    entities = [FavoriteNewsDto::class],
-    version = 1,
+    entities = [FavoriteNewsDto::class, NewsDto::class],
+    version = 2,
 )
-abstract class FavoriteNewsDatabase: RoomDatabase() {
+abstract class FavoriteNewsDatabase : RoomDatabase() {
 
     abstract val newsDao: FavoriteNewsDao
 
