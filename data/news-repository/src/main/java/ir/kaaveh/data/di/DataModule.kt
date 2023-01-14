992b1451bb881e4345a12b51d19306b479d2d68b
Baha <ali.bahaabadi@basalam.com>
2023-01-14 12:03:18 +0330

    [feature] make DataModule singleton.

--- data/news-repository/src/main/java/ir/kaaveh/data/di/DataModule.kt
+++ data/news-repository/src/main/java/ir/kaaveh/data/di/DataModule.kt
@@ -4,11 +4,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
+import dagger.hilt.components.SingletonComponent
 import ir.kaaveh.data.repository.NewsRepositoryImpl
 import ir.kaaveh.domain.repository.NewsRepository
 
 @Module
-@InstallIn(ActivityRetainedComponent::class)
+@InstallIn(SingletonComponent::class)
 interface DataModule {
 
     @Binds
