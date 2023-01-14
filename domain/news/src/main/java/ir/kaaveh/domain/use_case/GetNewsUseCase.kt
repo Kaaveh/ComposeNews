d6416d869d9d472f56ceb178b6d021c82f2490bc
Baha <ali.bahaabadi@basalam.com>
2023-01-14 10:51:28 +0330

    [feature] fix get news usecase to get data from db.

--- domain/news/src/main/java/ir/kaaveh/domain/use_case/GetNewsUseCase.kt
+++ domain/news/src/main/java/ir/kaaveh/domain/use_case/GetNewsUseCase.kt
@@ -1,25 +1,23 @@
 package ir.kaaveh.domain.use_case
 
 import ir.kaaveh.domain.model.News
-import ir.kaaveh.domain.model.Resource
 import ir.kaaveh.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetNewsUseCase @Inject constructor(
     private val repository: NewsRepository,
 ) {
 
-    operator fun invoke(): Flow<Resource<List<News>>> = flow {
-        try {
-            emit(Resource.Loading())
-            val newsList =
-                repository.getNews().map { it.copy(isFavorite = repository.isFavoriteNews(it)) }
-            emit(Resource.Success(newsList))
-        } catch (e: Exception) {
-            emit(Resource.Error(e))
-        }
-    }
+    operator fun invoke(): Flow<List<News>> =
+        repository.getNews()
+            .map { list ->
+                list.map { news ->
+                    news.copy(
+                        isFavorite = repository.isFavoriteNews(news)
+                    )
+                }
+            }
 
 }
