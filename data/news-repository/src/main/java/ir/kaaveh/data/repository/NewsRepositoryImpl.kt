05ffec71b078688581507931c1811c31803ede3d
Baha <ali.bahaabadi@basalam.com>
2023-01-14 10:50:50 +0330

    [feature] add new method for db to repo.

--- data/news-repository/src/main/java/ir/kaaveh/data/repository/NewsRepositoryImpl.kt
+++ data/news-repository/src/main/java/ir/kaaveh/data/repository/NewsRepositoryImpl.kt
@@ -5,6 +5,7 @@ import ir.kaaveh.domain.repository.NewsRepository
 import ir.kaaveh.localdatasource.database.FavoriteNewsDao
 import ir.kaaveh.localdatasource.mapper.toFavoriteNewsDto
 import ir.kaaveh.localdatasource.mapper.toNews
+import ir.kaaveh.localdatasource.mapper.toNewsDto
 import ir.kaaveh.remotedatasource.api.NewsApi
 import ir.kaaveh.remotedatasource.mapper.toNews
 import kotlinx.coroutines.flow.Flow
@@ -16,8 +17,20 @@ class NewsRepositoryImpl @Inject constructor(
     private val dao: FavoriteNewsDao,
 ) : NewsRepository {
 
-    override suspend fun getNews(): List<News> =
-        api.getNews().news.map { it.toNews() }
+    override fun getNews(): Flow<List<News>> =
+        dao.getAllNews().map { list -> list.map { it.toNews() } }
+
+    override suspend fun updateNews(): Boolean = try {
+        println("xxxxxxxxxxxxxxxx work")
+        api.getNews().news
+            .map { it.toNews().toNewsDto() }
+            .onEach {
+                dao.insertNews(it)
+            }
+        true
+    } catch (e: Exception) {
+        false
+    }
 
     override suspend fun insertFavoriteNews(news: News) =
         dao.insertFavoriteNews(news = news.toFavoriteNewsDto())
--- domain/news/src/main/java/ir/kaaveh/domain/repository/NewsRepository.kt
+++ domain/news/src/main/java/ir/kaaveh/domain/repository/NewsRepository.kt
@@ -4,7 +4,8 @@ import ir.kaaveh.domain.model.News
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
-    suspend fun getNews(): List<News>
+    fun getNews(): Flow<List<News>>
+    suspend fun updateNews(): Boolean
     suspend fun insertFavoriteNews(news: News)
     suspend fun removeFavoriteNews(news: News)
     suspend fun isFavoriteNews(news: News): Boolean
