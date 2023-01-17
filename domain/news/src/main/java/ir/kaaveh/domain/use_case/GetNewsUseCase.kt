3a64df503d9b013190e41ae44677682e69fb801f
Baha <ali.bahaabadi@basalam.com>
2023-01-17 19:59:21 +0330

    [feature] change get news use case.

--- domain/news/src/main/java/ir/kaaveh/domain/use_case/GetNewsUseCase.kt
+++ domain/news/src/main/java/ir/kaaveh/domain/use_case/GetNewsUseCase.kt
@@ -3,21 +3,12 @@ package ir.kaaveh.domain.use_case
 import ir.kaaveh.domain.model.News
 import ir.kaaveh.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetNewsUseCase @Inject constructor(
     private val repository: NewsRepository,
 ) {
 
-    operator fun invoke(): Flow<List<News>> =
-        repository.getNews()
-            .map { list ->
-                list.map { news ->
-                    news.copy(
-                        isFavorite = repository.isFavoriteNews(news)
-                    )
-                }
-            }
+    operator fun invoke(): Flow<List<News>> = repository.getNews()
 
 }
