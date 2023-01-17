ec849664b8a8cfcf76996d86182afbdb4e97a00a
Baha <ali.bahaabadi@basalam.com>
2023-01-17 19:54:32 +0330

    [feature] change database dao.

---
+++ data/news-local/src/main/java/ir/kaaveh/localdatasource/database/NewsDao.kt
@@ -0,0 +1,22 @@
+package ir.kaaveh.localdatasource.database
+
+import androidx.room.*
+import ir.kaaveh.localdatasource.dto.LocalNewsDto
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NewsDao {
+
+    @Query("SELECT * FROM news")
+    fun getAllNews(): Flow<List<LocalNewsDto>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNews(news: LocalNewsDto)
+
+    @Update(entity = LocalNewsDto::class)
+    suspend fun updateFavoriteNews(news: LocalNewsDto)
+
+    @Query("SELECT EXISTS(SELECT * FROM news WHERE title = :title AND source = :source AND isFavorite = :isFavorite)")
+    suspend fun isFavoriteNews(title: String, source: String, isFavorite: Boolean = true): Boolean
+
+}
