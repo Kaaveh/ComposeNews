a5421ad69932371815cf12acf3e43c07c03ebc9b
kaaveh <kaaveh@pm.me>
2023-01-30 17:43:53 +0330

    [feature] add test for db

--- data/news-local/build.gradle.kts
+++ data/news-local/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
 kapt {
---
+++ data/news-local/src/androidTest/java/ir/kaaveh/localdatasource/database/NewsDaoTest.kt
@@ -0,0 +1,66 @@
+package ir.kaaveh.localdatasource.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import ir.kaaveh.localdatasource.dto.LocalNewsDto
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class NewsDaoTest {
+    private lateinit var newsDao: NewsDao
+    private lateinit var db: NewsDatabase
+    private val localNewsDto = LocalNewsDto(
+        author = "",
+        description = "",
+        publishedAt = "",
+        source = "",
+        title = "",
+        url = "",
+        urlToImage = "",
+        isFavorite = true,
+    )
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, NewsDatabase::class.java).build()
+        newsDao = db.newsDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @kotlin.jvm.Throws(Exception::class)
+    fun emptyInitTable() = runTest {
+        val newsList = newsDao.getAllNews().first()
+        assertTrue(newsList.isEmpty())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertNews() = runTest {
+        newsDao.insertNews(localNewsDto)
+        val newsList = newsDao.getAllNews().first()
+        assertTrue(newsList.isNotEmpty())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun isFavoriteNews() = runTest {
+        newsDao.insertNews(localNewsDto)
+        assertTrue(newsDao.isFavoriteNews(title = localNewsDto.title, source = localNewsDto.source))
+    }
+
+}
