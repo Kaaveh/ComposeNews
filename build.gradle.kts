655cf54c4d990d65377906c5ebfe5869ef4a5809
koorosh <koorosh_mashhadi@yahoo.com>
2023-05-27 14:51:56 +0430

    update IDE + Java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
     kotlin("android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.44.2" apply false
 }
