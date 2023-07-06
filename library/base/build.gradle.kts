7523b7fe6f1e460df257ca7ffa2e13bead1ee9c1
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-07-06 17:20:55 +0330

    [feature] add base module

---
+++ library/base/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ir.kaaveh.base"
+    compileSdk = projectCompileSdkVersion
+
+    defaultConfig {
+        minSdk = projectMinSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = ComposeDependencies.kotlinCompilerExtensionVersion
+    }
+}
+
+dependencies {
+    
+    api(project(":library:core-test"))
+    implementation(project(":library:designsystem"))
+
+    LifeCycleDependencies.apply {
+        implementation(lifeCycleViewModelKtx)
+    }
+}
