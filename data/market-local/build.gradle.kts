eb14cdd45e828d8de55e6170e1c6c75553be4dd4
Vahid Garousi <garousiv@gmail.com>
2023-08-08 18:46:54 +0330

    Refactor data\market-local build.gradle.kts for #52

--- data/market-local/build.gradle.kts
+++ data/market-local/build.gradle.kts
@@ -1,45 +1,14 @@
 plugins {
-    libs.plugins.apply {
-        alias(android.library)
-        alias(kotlin.android)
-        alias(hilt.android)
-        alias(kapt)
-    }
+    id("composenews.android.library")
+    id("composenews.android.hilt")
 }
 
 android {
     namespace = "ir.kaaveh.localdatasource"
-    compileSdk = libs.versions.projectCompileSdkVersion.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.projectMinSdkVersion.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
 }
 
 dependencies {
     libs.apply {
-        implementation(hilt.android)
-        kapt(dager.hilt.compiler)
         implementation(room.runtime)
         implementation(room.ktx)
         kapt(room.compiler)
@@ -47,8 +16,4 @@ dependencies {
         androidTestImplementation(junit.ext)
         androidTestImplementation(coroutines.test)
     }
-}
-
-kapt {
-    correctErrorTypes = true
 }
