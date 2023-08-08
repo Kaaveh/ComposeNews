390195609dd0aba14adedb33ba8de87c417a4148
Vahid Garousi <garousiv@gmail.com>
2023-08-08 18:48:37 +0330

    Refactor feature\marketlist build.gradle.kts for #52

--- feature/marketlist/build.gradle.kts
+++ feature/marketlist/build.gradle.kts
@@ -1,45 +1,11 @@
 plugins {
-    libs.plugins.apply {
-        alias(android.library)
-        alias(kotlin.android)
-        alias(hilt.android)
-        alias(kapt)
-    }
+    id("composenews.android.library")
+    id("composenews.android.library.compose")
+    id("composenews.android.hilt")
 }
 
 android {
     namespace = "ir.kaaveh.marketlist"
-    compileSdk = libs.versions.projectCompileSdkVersion.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.projectMinSdkVersion.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
-    }
 }
 
 dependencies {
@@ -47,12 +13,6 @@ dependencies {
     api(project(":library:base"))
     implementation(project(":data:market-repository"))
     libs.apply {
-        implementation(hilt.android)
-        kapt(dager.hilt.compiler)
         implementation(hilt.navigation.compose)
     }
-}
-
-kapt {
-    correctErrorTypes = true
 }
