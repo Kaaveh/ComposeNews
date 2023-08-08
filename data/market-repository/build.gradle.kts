a72de4d9fdbee93c680176612a7fdb91675b1f6c
Vahid Garousi <garousiv@gmail.com>
2023-08-08 18:47:08 +0330

    Refactor data\market-repository build.gradle.kts for #52

--- data/market-repository/build.gradle.kts
+++ data/market-repository/build.gradle.kts
@@ -1,42 +1,10 @@
 plugins {
-    libs.plugins.apply {
-        alias(android.library)
-        alias(kotlin.android)
-        alias(hilt.android)
-        alias(kapt)
-    }
+    id("composenews.android.library")
+    id("composenews.android.hilt")
 }
 
 android {
     namespace = "ir.kaaveh.data"
-    compileSdk = libs.versions.projectCompileSdkVersion.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.projectMinSdkVersion.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    packaging {
-        resources.excludes.add("META-INF/*")
-    }
 }
 
 dependencies {
@@ -45,8 +13,6 @@ dependencies {
     implementation(project(":data:market-local"))
     libs.apply {
         implementation(lifecycle.viewmodel.ktx)
-        implementation(hilt.android)
-        kapt(dager.hilt.compiler)
         implementation(room.runtime)
         implementation(room.ktx)
         kapt(room.compiler)
@@ -55,8 +21,4 @@ dependencies {
         androidTestImplementation(coroutines.test)
         androidTestImplementation(runner)
     }
-}
-
-kapt {
-    correctErrorTypes = true
 }
