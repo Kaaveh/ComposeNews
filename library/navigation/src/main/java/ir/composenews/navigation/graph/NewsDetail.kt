43e68ad05a70924a094108695507fee784979bd4
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-08-22 09:55:26 +0330

    [feature] add animation into navigation to detail screen

--- library/navigation/src/main/java/ir/composenews/navigation/graph/NewsDetail.kt
+++ library/navigation/src/main/java/ir/composenews/navigation/graph/NewsDetail.kt
@@ -1,5 +1,9 @@
 package ir.composenews.navigation.graph
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import ir.composenews.base.BaseViewModel
@@ -13,6 +17,18 @@ fun NavGraphBuilder.marketDetail(
 ) {
     composable(
         route = Destinations.MarketDetailScreen().route,
+        enterTransition = {
+            slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Start
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseOut),
+                towards = AnimatedContentTransitionScope.SlideDirection.End
+            )
+        }
     ) { entry ->
         val market = entry.parcelableData<Market>(Destinations.MarketDetailScreen().market)
         MarketDetailRoute(
