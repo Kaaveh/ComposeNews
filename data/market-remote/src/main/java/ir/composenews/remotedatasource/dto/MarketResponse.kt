bd563b4d0a093ec3d6c580be8d88d7e013c99278
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2023-08-24 19:12:59 +0330

    [refactor] use kotlin serialization instead Gson serialization

--- data/market-remote/src/main/java/ir/composenews/remotedatasource/dto/MarketResponse.kt
+++ data/market-remote/src/main/java/ir/composenews/remotedatasource/dto/MarketResponse.kt
@@ -1,14 +1,16 @@
 package ir.composenews.remotedatasource.dto
 
-import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class MarketResponse(
     val id: String,
     val name: String,
     val symbol: String,
-    @SerializedName("current_price") val currentPrice: Double,
-    @SerializedName("market_cap") val marketCap: Double,
-    @SerializedName("total_volume") val totalVolume: Double,
-    @SerializedName("price_change_percentage_24h") val priceChangePercentage24h: Double,
-    @SerializedName("image") val imageUrl: String
+    @SerialName("current_price") val currentPrice: Double,
+    @SerialName("market_cap") val marketCap: Double,
+    @SerialName("total_volume") val totalVolume: Double,
+    @SerialName("price_change_percentage_24h") val priceChangePercentage24h: Double,
+    @SerialName("image") val imageUrl: String
 )
