c6c8e44d11af793a2f626aae5886e3fe8c4ed774
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-08-28 10:48:32 +0330

    [optimize] make domain model `stable`

--- domain/market/src/main/java/ir/composenews/domain/model/Market.kt
+++ domain/market/src/main/java/ir/composenews/domain/model/Market.kt
@@ -1,8 +1,10 @@
 package ir.composenews.domain.model
 
 import android.os.Parcelable
+import androidx.compose.runtime.Immutable
 import kotlinx.parcelize.Parcelize
 
+@Immutable
 @Parcelize
 data class Market(
     val id: String,
