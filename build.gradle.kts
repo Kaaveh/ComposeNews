4122f1d9859fd46dcb9c1a7fbd21167cd14232b1
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-08-28 08:35:10 +0330

    [feature] setup `composeCompilerReports`

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,23 @@ plugins {
         alias(kapt) apply false
     }
 }
+
+// Run it with: gradle assembleRelease -PcomposeCompilerReports=true
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            if (project.findProperty("composeCompilerReports") == "true") {
+                freeCompilerArgs += listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
+                )
+            }
+            if (project.findProperty("composeCompilerMetrics") == "true") {
+                freeCompilerArgs += listOf(
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
+                )
+            }
+        }
+    }
+}
