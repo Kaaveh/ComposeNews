09ecfac1f9da0d563bbb1bb1da831f38ac56c44f
sajjad dehnavi <dehnavi671@gmail.com>
2023-09-02 23:13:43 +0330

    object to data object sealed class `Destinations`

--- library/navigation/src/main/java/ir/composenews/navigation/Destinations.kt
+++ library/navigation/src/main/java/ir/composenews/navigation/Destinations.kt
@@ -1,9 +1,9 @@
 package ir.composenews.navigation
 
 sealed class Destinations(val route: String) {
-    object MarketListScreen : Destinations("market_list_screen")
+    data object MarketListScreen : Destinations("market_list_screen")
     data class MarketDetailScreen(val market: String = "market") :
         Destinations("market_detail_screen")
 
-    object FavoriteMarketScreen : Destinations("favorite_market_list_screen")
+    data object FavoriteMarketScreen : Destinations("favorite_market_list_screen")
 }
