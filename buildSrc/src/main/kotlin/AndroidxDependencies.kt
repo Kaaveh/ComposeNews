f2ed954ce70ef2409bed68d376c7075c9ba3e368
Mohammad Rezaei <moreat92@gmail.com>
2023-09-12 13:10:47 +0330

    [feat] Add DevicePosture interface

--- buildSrc/src/main/kotlin/AndroidxDependencies.kt
+++ buildSrc/src/main/kotlin/AndroidxDependencies.kt
@@ -2,7 +2,9 @@ object AndroidxDependencies {
 
     object Versions {
         const val coreKtx = "1.9.0"
+        const val window = "1.1.0"
     }
 
     const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val window = "androidx.window:window:${Versions.window}"
 }
--- buildSrc/src/main/kotlin/ComposeDependencies.kt
+++ buildSrc/src/main/kotlin/ComposeDependencies.kt
@@ -1,9 +1,10 @@
 object ComposeDependencies {
     object Version {
-        const val composeBOM = "2023.01.00"
+        const val composeBOM = "2023.06.01"
         const val composeActivity = "1.6.1"
         const val composeCoil = "2.2.2"
         const val accompanist = "0.24.11-rc"
+        const val adaptive = "0.26.2-beta"
     }
 
     const val kotlinCompilerExtensionVersion = "1.4.2"
@@ -17,5 +18,7 @@ object ComposeDependencies {
     const val composeCoil = "io.coil-kt:coil-compose:${Version.composeCoil}"
     const val accompanistWebview =
         "com.google.accompanist:accompanist-webview:${Version.accompanist}"
+    const val windowSizeClass = "androidx.compose.material3:material3-window-size-class"
+    const val accompanistAdaptive = "com.google.accompanist:accompanist-adaptive:${Version.adaptive}"
 
 }
