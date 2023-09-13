e75ba41e85d2be3a3bb1ad0d0020e6a79758045d
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2023-09-14 00:46:12 +0330

    [feature] Complete replacement of the theme, including color, shape, type into Material 3

--- library/designsystem/src/main/java/ir/composenews/designsystem/theme/Shape.kt
+++ library/designsystem/src/main/java/ir/composenews/designsystem/theme/Shape.kt
@@ -1,11 +1,11 @@
 package ir.composenews.designsystem.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Shapes
+import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    large = RoundedCornerShape(12.dp),
 )
