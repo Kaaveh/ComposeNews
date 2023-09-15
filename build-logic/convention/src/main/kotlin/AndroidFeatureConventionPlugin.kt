8539bf4d6104ac1069983541a21d4a60c844b245
Vahid Garousi <garousiv@gmail.com>
2023-09-15 13:40:58 +0330

    Rename AndroidPresentationLayerConventionPlugin.kt to AndroidFeatureConventionPlugin.kt for #63

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
-class AndroidPresentationLayerConventionPlugin : Plugin<Project> {
+class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply {
