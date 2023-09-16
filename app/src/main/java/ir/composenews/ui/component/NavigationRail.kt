d438a7dde64e30421bcbf7faa68721de581e6481
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2023-09-16 15:20:38 +0330

    [refactor] Add padding to "NavigationRail" component to prevent items from colliding with the "NavigationRail" when swiping

--- app/src/main/java/ir/composenews/ui/component/NavigationRail.kt
+++ app/src/main/java/ir/composenews/ui/component/NavigationRail.kt
@@ -1,10 +1,12 @@
 package ir.composenews.ui.component
 
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.NavigationRail
 import androidx.compose.material3.NavigationRailItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ir.composenews.designsystem.component.BottomNavigationIcon
 import ir.composenews.navigation.BottomNavItem
 
@@ -16,7 +18,8 @@ fun ComposeNewsNavigationRail(
     onItemClick: (BottomNavItem) -> Unit
 ) {
     NavigationRail(
-        modifier = modifier.fillMaxHeight(),
+        modifier = modifier.fillMaxHeight()
+            .padding(end = 30.dp),
     ) {
         items.forEach { item ->
             val selected = item.route == currentScreenRoute
