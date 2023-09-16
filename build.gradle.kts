6e3e172dcfeed3cf0c3a9d73d984306504f0f681
Vahid Garousi <garousiv@gmail.com>
2023-09-16 18:37:02 +0330

    add todo for kotliner plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
         alias(hilt.android) apply false
         alias(kapt) apply false
         id("org.jmailen.kotlinter") version "3.16.0" apply false
-//        alias(org.jmailen.kotlinter)
+//        alias(org.jmailen.kotlinter) // Todo fix this when this issue got response [https://github.com/jeremymailen/kotlinter-gradle/issues/343]
         alias(libs.plugins.detekt) apply false
     }
 }
