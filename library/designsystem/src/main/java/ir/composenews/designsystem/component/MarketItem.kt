5fa878ca7599246822bac0c8a58b3e514ac1ffc8
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2023-09-19 00:48:27 +0330

    [enhancement] Shimmer Effect for market item composables

--- library/designsystem/src/main/java/ir/composenews/designsystem/component/MarketItem.kt
+++ library/designsystem/src/main/java/ir/composenews/designsystem/component/MarketItem.kt
@@ -7,12 +7,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -174,6 +179,71 @@ private fun MarketItemPrev() {
                 onItemClick = {},
                 onFavoriteClick = {}
             )
+        }
+    }
+}
+
+@Composable
+private fun ShimmerMarketItem() {
+    Card(
+        modifier = Modifier
+            .wrapContentHeight()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(12.dp)),
+        shape = MaterialTheme.shapes.large,
+        colors = if (isSystemInDarkTheme().not()) CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background) else CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+                    .shimmerEffect()
+            )
+            Column(
+                modifier = Modifier.weight(1F)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(20.dp)
+                        .shimmerEffect()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .height(20.dp)
+                        .shimmerEffect()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ShimmerMarketListItem() {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(20) {
+            ShimmerMarketItem()
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun ShimmerMarketItemPrev() {
+    ComposeNewsTheme {
+        Surface {
+            ShimmerMarketItem()
         }
     }
 }
