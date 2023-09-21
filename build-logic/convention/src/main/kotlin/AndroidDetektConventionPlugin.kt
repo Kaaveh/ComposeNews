801abbce80d7d9fb0c70c0b1cbab8c6474685a68
Vahid Garousi <garousiv@gmail.com>
2023-09-21 12:08:22 +0330

    Adding AndroidDetektConventionPlugin.kt for  #85

--- build-logic/convention/src/main/kotlin/AndroidDetektConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidDetektConventionPlugin.kt
@@ -1,32 +1,18 @@
-import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import ir.composenews.configureDetekt
 import ir.composenews.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.named
 
 class AndroidDetektConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply(libs.findLibrary("detekt-gradlePlugin").get().get().group)
+            pluginManager.apply(
+                libs.findLibrary("detekt-gradlePlugin").get().get().group.toString()
+            )
             val extension = extensions.getByType<DetektExtension>()
-            extension.apply {
-                tasks.named<Detekt>("detekt") {
-                    reports {
-                        xml.required.set(true)
-                        html.required.set(true)
-                        txt.required.set(true)
-                        sarif.required.set(true)
-                        md.required.set(true)
-                    }
-                }
-            }
-            dependencies {
-                "detektPlugins"(libs.findLibrary("detekt-formatting").get())
-            }
+            configureDetekt(extension)
         }
     }
 }
