b5f607db08ed9a1aa362c218a03a8233965fc349
Kaaveh Mohamedi <Kaaveh@pm.me>
2023-11-07 12:57:18 +0330

    [test] `RoundToTwoDecimalPlacesTest`

--- core/extensions/src/test/java/ir/composenews/extensions/RoundToTwoDecimalPlacesTest.kt
+++ core/extensions/src/test/java/ir/composenews/extensions/RoundToTwoDecimalPlacesTest.kt
@@ -1,13 +1,11 @@
 package ir.composenews.extensions
 
+import io.kotest.core.spec.style.StringSpec
 import org.junit.Assert
-import org.junit.Test
 import java.math.BigDecimal
 
-class RoundToTwoDecimalPlacesTest {
-    @Test
-    fun testRoundToTwoDecimalPlaces() {
-        // Define a map of test cases with input numbers and their expected rounded results
+class RoundToTwoDecimalPlacesTest : StringSpec({
+    "Round to two decimal places" {
         val testCases = mapOf(
             0.12345 to "0.12",
             1.0 to "1.00",
@@ -16,10 +14,9 @@ class RoundToTwoDecimalPlacesTest {
             BigDecimal("1234.56789") to "1234.57",
         )
 
-        // Iterate through the test cases and assert the expected results
         testCases.forEach { (input, expected) ->
             val result = input.roundToTwoDecimalPlaces()
             Assert.assertEquals(expected, result)
         }
     }
-}
+})
