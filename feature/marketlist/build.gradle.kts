66a77f889a75cde351ee6904ad042b6281d40bf1
mahdiabbasian <mam-1371@outlook.com>
2024-03-22 16:43:28 +0330

    Revert "configuration set because catch conflict version of test runner, implementation espresso and compose ui test. note: please review classpath"
    This reverts commit 1b408a9c3831e0cecda0cb44a05da593995d96d7.
    
    better commit: configuration set because catch conflict version of test runner, implementation espresso and compose ui test. note: please review classpath

--- feature/marketlist/build.gradle.kts
+++ feature/marketlist/build.gradle.kts
@@ -6,18 +6,9 @@ android {
     namespace = "ir.composenews.marketlist"
 }
 
-configurations.all {
-    resolutionStrategy {
-        force("androidx.test:runner:1.4.0" )
-    }
-}
-
 dependencies {
     implementation(projects.data.marketRepository)
     implementation(libs.kotlinx.collections.immutable)
-    implementation(libs.espresso.core)
-    implementation(libs.compose.ui.test.manifest)
-    implementation(libs.compose.ui.test.junit4)
     implementation(projects.core.uimarket)
     implementation(projects.core.extensions)
 }
