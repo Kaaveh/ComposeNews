c8fcab8bd032c79f6bc10164d6bcbc9c70726e6a
mahdiabbasian <mam-1371@outlook.com>
2024-03-22 16:41:20 +0330

    configuration set because catch conflict version of test runner. note: please review classpath

--- feature/marketdetail/build.gradle.kts
+++ feature/marketdetail/build.gradle.kts
@@ -6,7 +6,16 @@ android {
     namespace = "ir.composenews.newsdetail"
 }
 
+configurations.all {
+    resolutionStrategy {
+        force("androidx.test:runner:1.4.0" )
+    }
+}
+
 dependencies {
     implementation(projects.data.marketRepository)
     implementation(projects.core.uimarket)
+    implementation(libs.espresso.core)
+    implementation(libs.compose.ui.test.manifest)
+    implementation(libs.compose.ui.test.junit4)
 }
