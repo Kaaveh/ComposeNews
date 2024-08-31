d18d7e905a11dba194721d4948f42bcd0db4f7b6
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2024-08-31 21:24:00 +0330

    [feature] Add HttpClient extensions and ApiResponse handling functions

---
+++ core/ktor/src/main/java/ir/composenews/ktor/HttpClientExtension.kt
@@ -0,0 +1,17 @@
+package ir.composenews.ktor
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.request
+import io.ktor.http.HttpMethod
+
+suspend inline fun <reified T> HttpClient.getApiResponse(
+    builder: HttpRequestBuilder,
+): ApiResponse<T> {
+    builder.method = HttpMethod.Get
+    return apiResponseOf { request(builder) }
+}
+
+suspend inline fun <reified T> HttpClient.getApiResponse(
+    block: HttpRequestBuilder.() -> Unit,
+): ApiResponse<T> = getApiResponse(HttpRequestBuilder().apply(block))
