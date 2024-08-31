4b5f1c57aecf9d2fa5ddd9c6dfec6fead16c41b9
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2024-08-31 21:22:09 +0330

    [feature] Add `ApiResponse` utilities and `StatusCode`

---
+++ core/ktor/src/main/java/ir/composenews/ktor/StatusCode.kt
@@ -0,0 +1,17 @@
+
+package ir.composenews.ktor
+
+/**
+ * https://docs.coingecko.com/reference/common-errors-rate-limit
+ */
+enum class StatusCode(val code: Int) {
+    Unknown(0),
+    BadRequest(400),
+    Forbidden(403),
+    RequestTimeout(408),
+    TooManyRequests(429),
+    InternalServerError(500),
+    ServiceUnavailable(500),
+    AccessDenied(500),
+    LimitRequest(10005),
+}
