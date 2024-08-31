58c078d7ae1f46f94b568d3d998d1272613ceaa6
Mohamad Rezaei <mohamadrezaei18@gmail.com>
2024-08-31 21:11:43 +0330

    [feature] Add ktor module

---
+++ core/ktor/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("composenews.android.library")
+    libs.plugins.apply {
+        alias(kotlinx.serialization)
+    }
+}
+
+android {
+    namespace = "ir.composenews.ktor"
+
+}
+
+dependencies {
+    libs.apply {
+        implementation(bundles.ktor)
+    }
+}
