93eb40ed58493719b53af024130a45cf812a5464
Vahid Garousi <garousiv@gmail.com>
2025-03-12 13:19:58 +0330

    #284 Refactor: Configure signingConfig for release build variant
    - Set the signing configuration for the release build variant to use the debug signing key.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            // To publish on the Play store a private signing key is required, but to allow anyone
+            // who clones the code to sign and run the release variant, use the debug signing key.
+            // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
+            signingConfig = signingConfigs.named("debug").get()
         }
     }
 }
