a735f30d438cdcb91d3810e2b2505d2ef8e3fac4
Kaaveh Mohamedi <Kaaveh@pm.me>
2026-02-19 05:53:12 +0330

    #301 Fix favorite tab (#305)

--- feature/marketlist/src/main/java/ir/composenews/marketlist/MarketListScreen.kt
+++ feature/marketlist/src/main/java/ir/composenews/marketlist/MarketListScreen.kt
@@ -50,9 +50,6 @@ fun MarketListRoute(
     val (state, event) = use(viewModel = viewModel)
     LaunchedEffect(key1 = Unit) {
         event.invoke(MarketListContract.Event.OnSetShowFavoriteList(showFavoriteList = showFavoriteList))
-        if (!showFavoriteList) {
-            event.invoke(MarketListContract.Event.OnGetMarketList)
-        }
     }
     MarketListScreen(
         state = state,
