69dfd721a17b0f2cb17af2d816dbfa1fc3d3c094
kaaveh <kaaveh@pm.me>
2026-02-19 08:19:19 +0330

    #292 Upgrade Kotest and do necessary changes

--- core/test/src/main/java/ir/composenews/core_test/MainCoroutineListener.kt
+++ core/test/src/main/java/ir/composenews/core_test/MainCoroutineListener.kt
@@ -2,21 +2,23 @@
 
 package ir.composenews.core_test
 
-import io.kotest.core.listeners.TestListener
+import io.kotest.core.listeners.BeforeSpecListener
+import io.kotest.core.listeners.AfterSpecListener
 import io.kotest.core.spec.Spec
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 
 class MainCoroutineListener(
     private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher(),
-) : TestListener {
+) : BeforeSpecListener, AfterSpecListener {
     override suspend fun beforeSpec(spec: Spec) {
         Dispatchers.setMain(testDispatcher)
     }
 
     override suspend fun afterSpec(spec: Spec) {
-        Dispatchers.setMain(testDispatcher)
+        Dispatchers.resetMain()
     }
 }
--- feature/marketdetail/src/test/java/ir/composenews/marketdetail/MarketDetailViewModelTest.kt
+++ feature/marketdetail/src/test/java/ir/composenews/marketdetail/MarketDetailViewModelTest.kt
@@ -37,7 +37,7 @@ class MarketDetailViewModelTest : StringSpec({
     val dispatcherProvider = TestDispatcherProvider(testScheduler)
     lateinit var viewModel: MarketDetailViewModel
 
-    listeners(MainCoroutineListener())
+    extensions(MainCoroutineListener())
 
     beforeEach {
         viewModel = MarketDetailViewModel(
