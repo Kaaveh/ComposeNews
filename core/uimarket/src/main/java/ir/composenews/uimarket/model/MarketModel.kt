ea3a38f68355f7d8dfe197958d20bf2c9cfdd44f
Kaaveh Mohamedi <Kaaveh@pm.me>
2026-04-13 08:56:28 +0330

    322 simplify `MarketModel` (#323)

--- core/uimarket/src/main/java/ir/composenews/uimarket/model/MarketModel.kt
+++ core/uimarket/src/main/java/ir/composenews/uimarket/model/MarketModel.kt
@@ -1,14 +1,9 @@
-@file:Suppress(
-    "ktlint:standard:function-expression-body",
-    "ktlint:standard:function-signature",
-    "ktlint:standard:trailing-comma-on-call-site"
-)
-
 package ir.composenews.uimarket.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class MarketModel(
     val id: String,
     val name: String,
@@ -17,38 +12,4 @@ data class MarketModel(
     val priceChangePercentage24h: Double,
     val imageUrl: String,
     val isFavorite: Boolean = false,
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty(),
-        parcel.readString().orEmpty(),
-        parcel.readDouble(),
-        parcel.readDouble(),
-        parcel.readString().orEmpty(),
-        parcel.readByte() != 0.toByte(),
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(name)
-        parcel.writeString(symbol)
-        parcel.writeDouble(currentPrice)
-        parcel.writeDouble(priceChangePercentage24h)
-        parcel.writeString(imageUrl)
-        parcel.writeByte(if (isFavorite) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MarketModel> {
-        override fun createFromParcel(parcel: Parcel): MarketModel {
-            return MarketModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MarketModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
