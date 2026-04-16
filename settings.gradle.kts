3b13dcec75daeb13e3253586645ea42b43b4729e
kaaveh <kaaveh@pm.me>
2026-04-16 06:59:48 +0330

    Conditional Maven repository inclusion in settings.gradle.kts #215
    Wrap the `https://maven.myket.ir` repository in a check for the `CI` environment variable. This ensures the custom Maven repository is only used during local development and skipped in CI environments.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,8 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.myket.ir") }
+        if (System.getenv("CI") == null) {
+            maven { url = uri("https://maven.myket.ir") }
+        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -15,7 +17,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.myket.ir") }
+        if (System.getenv("CI") == null) {
+            maven { url = uri("https://maven.myket.ir") }
+        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
