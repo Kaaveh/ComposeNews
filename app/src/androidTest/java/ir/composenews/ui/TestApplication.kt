c813dc3e4010f9b51ad623920077a712de95026a
Masoud <karimi.masoud5494@gmail.com>
2026-05-31 13:08:33 +0330

    [Fix] add missing stopKoin in TestApplication.kt

--- app/src/androidTest/java/ir/composenews/ui/TestApplication.kt
+++ app/src/androidTest/java/ir/composenews/ui/TestApplication.kt
@@ -2,6 +2,7 @@ package ir.composenews.ui
 
 import android.app.Application
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 
 class TestApplication : Application() {
     override fun onCreate() {
@@ -11,5 +12,10 @@ class TestApplication : Application() {
                 testModule,
             )
         }
+    }
+
+    override fun onTerminate() {
+        stopKoin()
+        super.onTerminate()
     }
 }
